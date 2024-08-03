55d963ede0758be8fab0a74e2fe746a0c6d3fdb3
Andrew Glushnev <glushnev22@gmail.com>
2024-08-03 19:39:47 +0700

    Создана структура проекта

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Landing from "./features/landing/Landing";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Landing />
+    </ChakraProvider>
   </React.StrictMode>
 );
